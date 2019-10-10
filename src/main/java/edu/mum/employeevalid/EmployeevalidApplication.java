95cb2d71e14e7a12cbafeef9da924724c9176845
GetanehYilma <gyletike@mum.edu>
2019-10-10 14:49:47 -0500

    EmployeeValid with Spring Boot and Thymleaf

---
+++ src/main/java/edu/mum/employeevalid/EmployeevalidApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.employeevalid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeevalidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeevalidApplication.class, args);
+    }
+
+}
