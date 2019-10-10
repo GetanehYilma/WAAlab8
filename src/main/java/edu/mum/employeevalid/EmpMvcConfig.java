accba944eda85595673b9a5c386f6a7a2be46f73
GetanehYilma <gyletike@mum.edu>
2019-10-10 18:18:41 -0500

    EmployeeValid with Spring Boot and Thymleaf

---
+++ src/main/java/edu/mum/employeevalid/EmpMvcConfig.java
@@ -0,0 +1,20 @@
+package edu.mum.employeevalid;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class EmpMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasenames("classpath:errorMessages", "classpath:message");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+}
